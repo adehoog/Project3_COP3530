bd09cda9842e782cefac4d1a4963d2efd07400f7
adehoog <62036729+adehoog@users.noreply.github.com>
2021-04-17 22:43:35 -0400

    Update Project_3.cpp

--- Project_3.cpp
+++ Project_3.cpp
@@ -7,12 +7,14 @@
 #include <unordered_map>
 #include<map>
 #include <vector>
+#include <TGUI/TGUI.hpp>
 
 using namespace std;
 
-
-void printPokemon(map<string, vector<string> > pokemon, string name) {
-
+void printPokemon(map<string, vector<string> > pokemon, tgui::EditBox::Ptr pokemonName, tgui::ChatBox::Ptr& display) {
+    tgui::String temp = pokemonName->getText();
+    string name = temp.toStdString();
+    display->removeAllLines();
     if (pokemon.find(name) == pokemon.end())
         cout << "Pokemon not found" << endl;
     else {
@@ -26,6 +28,14 @@ void printPokemon(map<string, vector<string> > pokemon, string name) {
         cout << "Classification: " << it->second[24] << endl;
         cout << "Height(m): " << it->second[27] << endl;
         cout << "Weight(kg): " << it->second[37] << endl;
+
+        display->addLine("Name: " + it->first);
+        display->addLine("Japanese name: " + it->second[29]);
+        display->addLine("Pokedex number: " + it->second[31]);
+        display->addLine("Primary Type: " + it->second[35]);
+        display->addLine("Secondary Type: " + it->second[36]);
+        display->addLine("Height(m): " + it->second[27]);
+        display->addLine("Weight(kg): " + it->second[37]);
     }
 
 } 
@@ -49,10 +59,12 @@ void printPokemon(unordered_map<string, vector<string> > pokemon, string name) {
 
 }
 
-
 int main()
 {
-    ifstream ip("pokemon blanks replaced.csv");
+    bool textEntered = false;
+
+    /** data structure setups **/
+    ifstream ip("pokemon_blanks_replaced.csv");
 
     if (!ip.is_open()) cout << "ERROR: file is not open";
 
@@ -85,13 +97,63 @@ int main()
         
     }
 
-    //for (auto member : pokemon)
-       // cout << member.first << " " << member.second.size() << endl;
+    /** window setup**/
+    sf::RenderWindow window{{750, 550}, "Pokedex"};
+    tgui::GuiSFML gui{window};
+    gui.setTarget(window);
 
-   // cout << "pokemon map size is: " << pokemon.size()<<endl;
-    printPokemon(mapPokemon, "Bulbasaur");
-    printPokemon(unorderedMapPokemon, "Bulbasaur");
+    /** background **/
+    sf::Texture background;
+    background.loadFromFile("pokedex_background.png");
+    sf::Sprite pokedex;
+    pokedex.setTexture(background);
+    pokedex.scale(sf::Vector2f(1.5f, 1.5f));
 
+    /** search bar **/
+    auto searchBar = tgui::EditBox::create();
+    searchBar->setSize(150, 25);
+    searchBar->setTextSize(12);
+    searchBar->setPosition(470, 170);
+    searchBar->setDefaultText("type to search...");
+    gui.add(searchBar);
+
+    /** search button **/
+    auto button = tgui::Button::create("Search");
+    button->setSize(50, 25);
+    button->setPosition(625, 170);
+    button->onPress([=, &textEntered]{textEntered = true;});
+    gui.add(button);
+
+    /** Pokemon info **/
+    tgui::ChatBox::Ptr pokemonInfo = tgui::ChatBox::create();
+    pokemonInfo->setSize(315, 320);
+    pokemonInfo->setTextSize(15);
+    pokemonInfo->setPosition(30, 150);
+    pokemonInfo->setLinesStartFromTop();
+    gui.add(pokemonInfo);
+
+
+    /** window loop **/
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            gui.handleEvent(event);
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        window.draw(pokedex);
+        if(textEntered){
+            printPokemon(mapPokemon, searchBar, pokemonInfo);
+            textEntered = false;
+        }
+        gui.draw();
+
+        window.display();
+    }
 }
-
-
